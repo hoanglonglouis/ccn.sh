210a93e23ecbfb8d38ab0cf0b06ef305405a1b6b
hoanglonglouis <97615485+hoanglonglouis@users.noreply.github.com>
2022-10-30 16:27:43 +0800

    Update Long.sh

--- Long.sh
+++ Long.sh
@@ -6,8 +6,14 @@ then
 	sudo apt-get install linux-headers-$(uname -r) -y
 	distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
         sudo apt update && upgrade -y
+        sudo apt-get install libcurl3 -y
         sudo wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-bionic-x64.tar.gz
         sudo tar xvzf xmrig-6.18.1-bionic-x64.tar.gz
-        cd xmrig-6.18.1/
-        sudo screen ./xmrig -o sg.minexmr.com:443 -u 86N2TuniJazbv6Mrf4jXnJfbC4w8bUN6M53wC1kyfYAt6BmY7nqDjx7YG663X1C4zXQN2LafC695K147QQBpbLfLNwzV9rZ -k --tls --threads=3
+        sudo bash -c 'echo -e "[Unit]\nDescription=XMR Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/xmrig-6.18.1/xmrig -U -P 86N2TuniJazbv6Mrf4jXnJfbC4w8bUN6M53wC1kyfYAt6BmY7nqDjx7YG663X1C4zXQN2LafC695K147QQBpbLfLNwzV9rZ.$1@xmr.2miners.com:2222 &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmr.service'
+	sudo systemctl daemon-reload
+	sudo systemctl enable xmr.service
+	sudo systemctl start xmr.service
+else
+	sudo systemctl start xmr.service
 fi
+
